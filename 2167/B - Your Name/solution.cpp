1d6ffb8533d1a12e00666e164471844821e07363
Ansh Tayal <tayalansh176@gmail.com>
2025-11-14 12:58:40 +0530

    Add Your Name [B] from Codeforces

---
+++ 2167/B - Your Name/solution.cpp
@@ -0,0 +1,1 @@
+#include<iostream>#include<string>#include<algorithm>using namespace std;int main(){    int t;    cin>>t;    while(t--){        int n;        cin>>n;        string s,t;        cin>>s>>t;        sort(s.begin(),s.end());        sort(t.begin(),t.end());        if(s==t){            cout<<"YES"<<endl;        }        else{            cout<<"NO"<<endl;        }    }}
