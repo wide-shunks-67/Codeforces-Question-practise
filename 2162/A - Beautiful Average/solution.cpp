b8dadf6e981541c2b4a26da328704ea01c02e09f
Ansh Tayal <tayalansh176@gmail.com>
2025-11-15 17:19:36 +0530

    Add Beautiful Average [A] from Codeforces

---
+++ 2162/A - Beautiful Average/solution.cpp
@@ -0,0 +1,1 @@
+#include<iostream>#include<vector>#include<algorithm>using namespace std;int main(){    int t;    cin>>t;    while(t--){        int n;        cin>>n;        vector<int>arr(n);        for(int i=0;i<n;i++){            cin>>arr[i];        }        sort(arr.begin(),arr.end());        cout<<arr[n-1]<<endl;    }}
