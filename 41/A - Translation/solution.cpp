eab3d75c4c7191179672a2012cafa8c887f72108
Ansh Tayal <tayalansh176@gmail.com>
2025-11-17 23:13:42 +0530

    Add Translation [A] from Codeforces

---
+++ 41/A - Translation/solution.cpp
@@ -0,0 +1,1 @@
+#include<iostream>#include<string>#include<algorithm>using namespace std;int main(){    string s;    string t;    cin>>s;    cin>>t;    reverse(t.begin(),t.end());    if(t==s){        cout<<"YES";    }    else {        cout<<"NO";    }    return 0;}
