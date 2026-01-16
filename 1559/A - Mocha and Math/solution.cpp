98f3c7abebb7a2f03e36e3d64bca4d3c59494d15
Ansh Tayal <tayalansh176@gmail.com>
2026-01-16 13:31:30 +0530

    Add Mocha and Math [A] from Codeforces

---
+++ 1559/A - Mocha and Math/solution.cpp
@@ -0,0 +1,1 @@
+#include<iostream>#include<vector>#include<climits>#define ll long longusing namespace std; int main(){    int t;    cin>>t;    while(t--){        int n;        cin>>n;        vector<ll> a(n);                for(int i=0;i<n;i++){            cin>>a[i];        }        ll ans= a[0];        for(int i=1;i<n;i++){            ans= ans & a[i];        }        cout<<ans<<"\n";    } }
