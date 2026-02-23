ac1edb060a0b36575973cc748a0bdb95a5f477c9
Ansh Tayal <tayalansh176@gmail.com>
2026-02-23 20:22:27 +0530

    Add Parkour Design [A] from Codeforces

---
+++ 2202/A - Parkour Design/solution.cpp
@@ -0,0 +1,1 @@
+#include<iostream>using namespace std;int main(){    ios_base::sync_with_stdio(false);    cin.tie(NULL);    int t;    cin>>t;    while(t--){        int x,y;        cin>>x>>y;        if ((x - 2 * y) % 3 == 0 && x >= 2 * y && x >= -4 * y) {            cout << "YES\n";        } else {            cout << "NO\n";        }    }}
