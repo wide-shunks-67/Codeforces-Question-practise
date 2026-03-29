0688048d7be71cf857bc6c3d2aed8510c9ca95c9
Ansh Tayal <tayalansh176@gmail.com>
2026-03-29 20:14:41 +0530

    Add A Simple Sequence [A] from Codeforces

---
+++ 2210/A - A Simple Sequence/solution.cpp
@@ -0,0 +1,1 @@
+#include<iostream>#include<vector>using namespace std; int main(){    ios_base::sync_with_stdio(false);    cin.tie(NULL);    int t;    cin>>t;    while(t--){        int n;        cin>>n;        for(int i=0;i<n;i++){            cout<<( n - i)<<" ";        }        cout<<"\n";    } }
