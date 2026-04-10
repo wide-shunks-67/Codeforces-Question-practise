56d2a5d45c9a391be2f20ba38ce8aba71931bf6c
Ansh Tayal <tayalansh176@gmail.com>
2026-04-10 21:46:44 +0530

    Add Phone numbers [B] from Codeforces

---
+++ 25/B - Phone numbers/solution.cpp
@@ -0,0 +1,1 @@
+#include<iostream>#include<vector>#include<climits>using namespace std; int main(){    ios_base::sync_with_stdio(false);    cin.tie(NULL);    int n;    cin>>n;      string a;    cin>>a;    for(int i=0;i<n;i++){            cout<<a[i] ;        if(i%2==1 &&i<n-2){            cout<<"-";        }    } }
